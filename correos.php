973e351998c55fecb73e61dd1ce2d310039de0f5
ignaciosoraka <ignaciosoraka@gmail.com>
2023-10-05 09:59:57 -0300

    arreglos

--- correos.php
+++ correos.php
@@ -1,20 +1,28 @@
 <?php
-    $destinatario = 'ignaciosoraka@gmail.com';
+$destinatario = 'ignaciosoraka@gmail.com';
 
-    $nombre = $_POST['nombre'];
-    $asunto = $_POST['asunto'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
+$nombre = $_POST['nombre'];
+$ciudad = $_POST['ciudad']; // Cambio el nombre del campo
+$phone = $_POST['phone'];
+$email = $_POST['email'];
 
-    $header = "Desde: La pagina de ZANE " . $email; // Cambia "Enviado desde la página de ZANE" por el email del remitente
-    $mensaje = "Mensaje de contacto de: " . $nombre . "\n";
-    $mensaje .= "Asunto: " . $asunto . "\n";
-    $mensaje .= "Teléfono: " . $phone;
+$mensaje = "Mensaje de contacto:\n\n";
+$mensaje .= "Nombre: " . $nombre . "\n";
+$mensaje .= "Ciudad: " . $ciudad . "\n"; // Utiliza el valor de la ciudad como asunto
+$mensaje .= "Correo Electrónico: " . $email . "\n";
+$mensaje .= "Teléfono: " . $phone;
 
-    if (mail($destinatario, $asunto, $mensaje, $header)) {
-        echo "<script>alert('Correo enviado exitosamente');</script>";
-        echo "<script>setTimeout(\"location.href='index.html'\", 1000);</script>";
-    } else {
-        echo "<script>alert('Error al enviar el correo');</script>";
-    }
+$asuntoCorreo = "Consulta de " . $nombre; // Puedes mantener el asunto como "Consulta de [nombre]"
+
+$header = "From: ZANE <" . $email . ">\r\n";
+$header .= "Reply-To: " . $email . "\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+if (mail($destinatario, $asuntoCorreo, $mensaje, $header)) {
+    echo "<script>alert('Correo enviado exitosamente');</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\", 1000);</script>";
+} else {
+    echo "<script>alert('Error al enviar el correo');</script>";
+}
 ?>
